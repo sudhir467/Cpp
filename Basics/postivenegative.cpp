d01e473a4b1e2be682134b4d118ed4c29ea7a9c2
sudhir borra <sudhirborra6163@gmail.com>
2022-05-24 16:47:06 +0200

    postivenegative

---
+++ Basics/postivenegative.cpp
@@ -0,0 +1,27 @@
+/* This program takes number and prints whether its positive, negative or its zero*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << " Enter the value of a :";
+	cin>>a;
+	if(a > 0)
+	{
+		cout << " a is postive"<< endl;
+	}
+	else if(a<0)
+	{
+		cout << " a is negative " << endl;
+	}else
+	{
+		cout << "a is equal to zero"<< endl;
+	}
+	
+	return 0;
+}
+
+
+

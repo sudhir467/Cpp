a5afdef174827e09f406f3c2d0ad06b9ffe7321d
sudhir borra <sudhirborra6163@gmail.com>
2022-05-24 16:24:16 +0200

    basics

---
+++ Basics/comparetwonumbers.cpp
@@ -0,0 +1,22 @@
+// This program is used to compare two numbers and return the number which is greater
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout << "Enter the value of a:";
+	cin >> a;
+	cout << " Enter the value of b:";
+	cin >> b;
+
+	if (a>b)
+	{
+		cout << "a is greater than b"<< endl;
+	}else
+	{
+		cout << "b is greater than a" << endl;
+	}
+	return 0;
+}

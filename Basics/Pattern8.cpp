6e5fbb1c37d811f1ff0ca2ec16e526abedf2e4fd
sudhir borra <sudhirborra6163@gmail.com>
2022-11-15 00:05:02 +0100

    Pattern

--- Basics/Pattern8.cpp
+++ Basics/Pattern8.cpp
@@ -1,9 +1,9 @@
 /*This program is to print the following pattern
 A
-BB
-CCC
-DDDD
-EEEEE
+B B
+C C C
+D D D D
+E E E E E
 */
 
 #include<iostream>
@@ -13,15 +13,15 @@ int main()
 {
     int rows,cols,n;
     char ch='A';
-    cout<<"Enter the number of rows required:"<<endl;
-    cin>>n;
+    cout <<"Enter the number of rows required:"<<endl;
+    cin >> n;
     for(rows=1;rows<=n;rows++)
     {
         for(cols=1;cols<=rows;cols++)
         {
-            cout<< ch<<" ";
+            cout<< ch <<" ";
         }
-        cout<<endl;
+        cout << endl;
         ch++;
     }
 

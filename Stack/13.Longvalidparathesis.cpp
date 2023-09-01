7ed8e2dcbff2ace2b02e5aca0b89a6502fcb4307
Your <sudhirborra6163@gmail.com>
2023-09-01 13:14:03 +0200

    valid paranthesis

---
+++ Stack/13.Longvalidparathesis.cpp
@@ -0,0 +1,45 @@
+/* This program is to find the longest valid paranthesis*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int longestvalidparanthesis(string &str)
+{
+    stack<char> st;
+    int maxlen = 0;
+    st.push(-1);
+    for(int i=0;i<str.length();i++)
+    {
+        char ch=str[i];
+        if(ch=='(')
+        {
+            st.push(i);
+        }
+        else
+        {
+            st.pop();
+            if(st.empty())
+            {
+                st.push(i);
+            }
+            else
+            {
+                int len=i-st.top();
+                maxlen=max(len,maxlen);
+
+            }
+        }
+
+    }
+    return maxlen;
+
+}
+
+
+int main()
+{
+    string str = ")))()";
+    int max = longestvalidparanthesis(str);
+    cout << "longest valid paranthesis:" << max << endl;
+    return 0;
+}
